eb3cfdddc98705266d11350fa422f120513736da
Restioson <restiosondev@gmail.com>
2020-06-06 20:25:06 +0200

    doc

--- README.md
+++ README.md
@@ -25,8 +25,10 @@ Since spaad uses nightly features such as GATs and Type Alias Impl Trait, those 
 are the same features generally required for xtra.
 
 spaad exports one thing: the proc macro `spaad::entangled`. This is used as an attribute on the actor struct definition,
-`Actor` implementation, and on an impl block in which the handler functions are used. For example:
+`Actor` implementation, and on an impl block in which the handler functions are used.
 
+
+## Example 
 ```rust
 #![feature(type_alias_impl_trait, generic_associated_types)]
 
@@ -77,4 +79,23 @@ The proc macro will create the given struct (in the example's case, `Printer`). 
 address to the actual structure, which is strictly internal and cannot be interacted with except by sending messages.
 
 For a more complex example, such as handling the actor's disconnection and taking `Context` in a handler, see the
-documentation or `complex.rs` in the examples directory.
+documentation or `complex.rs` in the examples directory. To see the generated code, run `cargo +nightly doc` in the 
+`example_generated` folder.
+
+## Advantages
+
+- More ergonomic and concise.
+- IDE support. It is possible in some IDEs (only tested on IntelliJ IDEA with the Rust plugin) to jump to definition.
+  This is only partial, as in some cases the IDE does not understand the change in the mutability of the `self` parameter
+  (only `&self` is required when sending a message, but it looks as though it is declared `&mut self`).
+- Harder to cause undefined behaviour in xtra. In xtra, it is possible to cause undefined behaviour by using an
+  ill-typed type parameter as the `Responder` type in the `Handler` trait 
+  (i.e `impl<R> Handler<...> for ... { type Responder = R ... }`). However, since spaad defines this for the user, this
+  is not possible. To my (Restioson's) knowledge, this is the only way to cause UB in xtra (please PR a change to the
+  documentation with any other ways!)
+
+## Disadvantages
+
+- Similar caveats to xtra itself.
+- IDE support is not full. In some cases, there can be issues. This could be resolved by proc macro expansion, but that
+  appears to be a way off.
--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,74 @@ use syn::parse_macro_input;
 use syn::parse::{Parse, ParseStream};
 use std::ops::Deref;
 
+/// The main item of the crate. This is a proc macro used as an attribute on the actor struct
+/// definition, `Actor` implementation, and on an impl block in which the handler functions are used.
+///
+/// For example:
+///
+/// ```rust,ignore
+/// #![feature(type_alias_impl_trait, generic_associated_types)]
+///
+/// #[spaad::entangled]
+/// struct Printer {
+///     times: usize,
+/// }
+///
+/// #[spaad::entangled]
+/// impl Actor for Printer {
+///     fn started(&mut self, _ctx: &mut Context<Self>) {
+///         println!("Actor started!");
+///     }
+/// }
+///
+/// #[spaad::entangled]
+/// impl Printer {
+///     // New is a special case: if it is present, create() and spawn() functions are emitted too
+///     fn new() -> Self {
+///         // You must use Self when referring to the type itself, as it is internally renamed
+///         // something else. This allows for better IDE support.
+///         Self { times: 0 }
+///     }
+///
+///     async fn print(&mut self, string: String) {
+///         self.times += 1;
+///         println!("Printing {}. Printed {} times so far.", string, self.times);
+///     }
+/// }
+/// ```
+///
+/// It is important to note that the `new` function is a special case. If it is present, the proc
+/// macro will also emit a `create` method for the actor wrapper. It can take arguments. If the
+/// `with-tokio-0_2` or `with-async_std-1_0` features are enabled, then it will also emit a `spawn`
+/// method. These are both very similar to the `xtra::Actor` methods of the same names.
+///
+/// Messages can then be sent to actors as such:
+/// ```rust,ignore
+/// my_actor.print().await;
+/// ```
+///
+/// The output type of the future will be determined by the signature. It will be identical to the
+/// written type, except when the return is written as `Result<T, xtra::Disconnected>` (see below).
+///
+/// If you do not want to `await` for the message to complete processing, you can do the following:
+/// ```rust,ignore
+/// let _ = my_actor.print(); // Binding to avoid #[must_use] warning on Future
+/// ```
+///
+/// This will also mean that the return type will be discarded, as the receiving end of the channel
+/// will be dropped.
+///
+/// The proc macro will create the given struct (in the example's case, `Printer`). This is just a
+/// wrapper for an actor  address to the actual structure, which is strictly internal and cannot be
+/// interacted with except by sending messages. This is why `Self` must be used, and not the
+/// actual name of the actor struct. Currently, it is renamed to `__{Actor}`, but this is not stable
+/// and could change in the future.
+///
+/// The methods to send messages will panic if the actor is disconnected. If you want to manually
+/// handle this error, make the return type of the handler function `Result<T, xtra::Disconnected>`.
+/// The type must be named `Context` - it cannot be renamed by re-importing. If you want to access
+/// the actor cotnext add an argument to the function with `&mut Context<Self>` as the type.
+/// Similarly, the type must be named `Context` - it cannot be renamed by re-importing.
 #[proc_macro_attribute]
 pub fn entangled(_args: TokenStream, input: TokenStream) -> proc_macro::TokenStream {
     entangle(input)
